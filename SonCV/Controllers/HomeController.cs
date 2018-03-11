c429ecae9b22ebf7a720b34c4e45db683b20e823
atabey <issues109@gmail.com>
2018-03-11 21:53:11 +0300

    Initial commit

---
+++ SonCV/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SonCV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("hakkimda")]
+        public ActionResult Hakkimda()
+        {
+            return View();
+        }
+        [Route("deneyimlerim")]
+        public ActionResult Deneyimlerim()
+        {
+            return View();
+        }
+        [Route("egitimim")]
+        public ActionResult Egitimim()
+        {
+            return View();
+        }
+        [Route("becerilerim")]
+        public ActionResult Becerilerim()
+        {
+            return View();
+        }
+        [Route("hobilerim")]
+        public ActionResult Hobilerim()
+        {
+            return View();
+        }
+        [Route("kazanimlarim")]
+        public ActionResult Kazanimlarim()
+        {
+            return View();
+        }
+    }
+}
---
+++ SonCV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SonCV.MvcApplication" Language="C#" %>
